17aa2e87cb07ca2a1709cb22067f1acff60677bf
Douglas Rodrigo Ferreira <douglasrodrigo@gmail.com>
2012-11-06 18:55:59 -0200

    estrutura inicial do gerkart com rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gerkart::Application.config.secret_token = 'cbc0c7c1bdd244f053dc575820599f99b8a01267a25f4dfc42e1700781648316f03478a3b3ee949db71f220dda704b48f0475ccafe0ac534e2ba4f6d8d41d513'

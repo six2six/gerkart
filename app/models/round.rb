c4b8ed60cdd92e4ebca53ddcedcd8b00a18976d3
Arthur Hirata <arthur_hirata@yahoo.com.br>
2013-01-26 09:00:07 -0200

    fix: ordering roundPositions by position

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,7 +2,7 @@ class Round < ActiveRecord::Base
   attr_accessible :date, :name
   belongs_to :championship
   belongs_to :track
-  has_many :roundPositions
+  has_many :roundPositions, :order => 'round_positions.position'
   has_many :drivers, :through => :roundPositions
   accepts_nested_attributes_for :roundPositions
 
